e43a68c082f9c9bdbbd5b7250d9a1981c922adab
firened <fire@whatimean.ch>
2022-07-06 17:06:24 +0200

    init mqtt

--- battery_mqtt.py
+++ battery_mqtt.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import subprocess
 import socket
+import paho.mqtt.client as mqtt
 print(socket.gethostname())
 
 # get voltage
@@ -26,4 +27,28 @@ voltage = uV_volts / 1000
 # output the voltage
 print(voltage)
 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    publish(socket.gethostname(), voltage, qos=0, retain=False)
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    # client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+#def on_message(client, userdata, msg):
+#    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+#client.on_message = on_message
+
+client.connect("192.168.1.44", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+#client.loop_forever()
 
